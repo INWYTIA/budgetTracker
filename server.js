905749f5b6dbf569e97ccd017ea9188f3720443d
INWYTIA <inwytia@gmail.com>
2020-05-13 15:36:40 -0500

    indexdb integrated

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const Transaction = require("./models/transaction.js");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// require("./routes/api.js")(app);
 app.use(logger("dev"));
 
 app.use(compression());
@@ -21,7 +23,35 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes here
-require("./routes/api.js");
+app.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+app.post("/api/transaction/bulk", ({body}, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+app.get("/api/transaction", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
